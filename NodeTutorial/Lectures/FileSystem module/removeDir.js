12430393f12f59ae7c95fed81810347fc5de6c0b
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-05-25 20:51:31 +0530

    updated

--- NodeTutorial/Lectures/FileSystem module/removeDir.js
+++ NodeTutorial/Lectures/FileSystem module/removeDir.js
@@ -3,7 +3,7 @@ import * as fs from "fs/promises";
 //Remove Folder / Directory
 try{
     //await fs.rmdir("D:\\Desktop\\html\\File"); should be a directory only here File.txt is not a directory so gives a error.
-    await fs.rmdir("D:\\Desktop\\html\\courses");
+    await fs.rmdir("D:\\Desktop\\html\\courses");//the directory to be removed should be empty.
     console.log("Removed");
 }
 catch(error)
