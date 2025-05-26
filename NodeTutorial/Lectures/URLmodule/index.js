2fe83c117c670a2a214a267df477d9e358efb611
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-05-26 12:39:43 +0530

    updating a url

--- NodeTutorial/Lectures/URLmodule/index.js
+++ NodeTutorial/Lectures/URLmodule/index.js
@@ -12,3 +12,21 @@ console.log(myurl.searchParams); //here search parameter is 'query'=>'string'. h
 //BOTH WORKS THE SAME
 console.log(myurl.toString());  //url to string
 console.log(myurl.toJSON());  //url to JSON
+
+//setting in a url
+const myURL = new URL('https://example.org');
+myURL.password='abc@123';
+myURL.pathname='/a/b/c';
+myURL.search='?d=e';
+myURL.hash='#fgh';
+console.log(myURL.href);
+
+//now on this newly constructed url
+console.log(myURL.hash); //gives #fgh
+console.log(myURL.host); //gives hostname along with the the port.
+console.log(myURL.hostname); //gives the host name www.example.com
+console.log(myURL.port); //gives the port no is null
+console.log(myURL.password); // gives the password of the url 
+console.log(myURL.protocol); //protocol is: https here
+console.log(myURL.search); // gives the query to search here ?query=string
+console.log(myURL.searchParams);
