c3d1b18ac437db4b4c56594645b9eeb0fc073795
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-05-26 20:32:45 +0530

    http module updated

--- NodeTutorial/Lectures/HTTP module/index.js
+++ NodeTutorial/Lectures/HTTP module/index.js
@@ -2,9 +2,13 @@ import http from 'http';
 
 const server=http.createServer((req,res)=>{console.log(req);
     res.write(<h1>Hello from Node.js server!</h1>)
-    res.setHeader("Content-Type","Text/html");
-    res.statusCode=404;
-    res.statusMessage="BAD SERVER";
+    //res.setHeader("Content-Type","Text/html");
+   // res.statusCode=404;
+   // res.statusMessage="BAD SERVER";
+
+   //shorthand for status(cose,message,header)
+   res.write(202,"GOOD",{"Content-Type":"text/html"})
+
 });
 server.listen(8000,()=>console.log("server up"));
 
