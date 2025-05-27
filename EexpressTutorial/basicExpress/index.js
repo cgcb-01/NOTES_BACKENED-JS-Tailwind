07b6102b1083bc1277190017b2bdba3f23dc1dd0
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-05-28 01:18:46 +0530

    Advance routing and regex

--- EexpressTutorial/basicExpress/index.js
+++ EexpressTutorial/basicExpress/index.js
@@ -19,7 +19,26 @@ app.get("/about",(req,res)=>{
 })
 
 //String Pattern Pattern
-app.get("/ab?cd",(req,res)=>{
+/*app.get("/ab?cd",(req,res)=>{
     res.send("<h1>Will only work if either b or c or both bc is there in the url</h1>")
+})*/ //Not Allowed in newer version of Express js
+
+//The Above one can be implemented by regex
+//regex- we can have varoius regular expressions in the url
+//if the url has any of the following pattern then that page will be shown
+
+//REGEX
+app.get(/x/,(req,res)=>{
+  res.send("<h1>Will only work if x is there in the url</h1>")
+});
+
+app.get(/^\/users\/[0-9]{4}$/,(req,res)=>{
+   res.send("<h1>Will only work if users and / 4 nos after users is there in the url</h1>") 
 })
-app.listen(8000,()=>console.log("Server Up"));n
+
+//NESTED ROUTING
+app.get("/products/product/product-details",(req,res)=>{
+   res.send("<h1>Will only work if the url is: /products/product/product-details</h1>") 
+})
+
+app.listen(8000,()=>console.log("Server Up"));
