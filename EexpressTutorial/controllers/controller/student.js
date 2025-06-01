05f085fc200079d05d209f3e7066ead9fcad93c7
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-01 21:34:08 +0530

    controllers

--- EexpressTutorial/controllers/controller/student.js
+++ EexpressTutorial/controllers/controller/student.js
@@ -1,4 +1,6 @@
 import express from "express"
+// here we are defining all what we have to send as res.send when the router works.
+//This is why we have defined another file as controller
 const allStudents = (req,res)=>res.send('All Students');
 const newStudent=(req,res)=>res.send('Create new Student');
 const updateStudent=(req,res)=>res.send('Update a Student')
