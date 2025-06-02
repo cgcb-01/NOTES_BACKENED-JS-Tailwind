969605eaf4068d02692b01a05c0d57c2a7292e8f
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-02 23:09:25 +0530

    serving static files

--- EexpressTutorial/Serving Static Files/index.js
+++ EexpressTutorial/Serving Static Files/index.js
@@ -11,3 +11,13 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(process.cwd(),"./static_file/index.html")) //process.cwd is used to mention the currect directory
 });
 app.listen(8000,()=>console.log("Server up"));
+
+//WORKING OF IT:
+/*Here the express router first serves the static files.
+Means any static files if present will be processed first. 
+
+Here so we mentioned the static_file directory and then we are sending the index.html file to the express server.
+
+app.use is mentioned earlier to serve it first if any static files.
+
+Instead of mentioning only directory if we had mentioned the path of index.htmlthen only index.html will be used.*/ 
