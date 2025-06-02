3ec8e7176c9ff05bec6cc50ae3120d712267ce1a
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-06-02 20:04:52 +0530

    middleware

---
+++ EexpressTutorial/Middleware/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+const app= express();
+/*function userCredentials(req,res,next){
+    console.log("username: (alex)");
+    console.log("email: (alex21@gmail.com)");
+    console.log("Age: (19)");
+    next();
+}*/
+//We can define it here or in a seperate folder like middle ware and import it here.
+import userCredentials from "./middleware/log";
+/*
+- Request
+- Middleware
+- Response
+*/
+app.get("/",userCredentials,(req,res)=>{
+    res.send("<h1> Hello Admin</h1>");
+});
+
+app.listen(8000,()=>console.log("Server Up"));
---
+++ EexpressTutorial/Middleware/middleware/log.js
@@ -0,0 +1,7 @@
+function userCredentials(req,res,next){
+    console.log("username: (alex)");
+    console.log("email: (alex21@gmail.com)");
+    console.log("Age: (19)");
+    next();
+}
+export default userCredentials;
