d2ce587448fae2391384ef3ed34bd5bfcc6c1ef6
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-08-20 05:04:36 +0530

    Updated

--- Redux-toolkit_Notes/redux-tolkit/src/app/service/dummyData.js
+++ Redux-toolkit_Notes/redux-tolkit/src/app/service/dummyData.js
@@ -1,4 +1,4 @@
-import { createApi, fetcbaseQuery } from "reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "reduxjs/toolkit/query/react";
 createApi({
   reducerPath: "products",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dummyhson.com" }), // here we need o mention the ase url from where the data will be fetched. The api build by express and MongoDB but here we will use a Dummy API
