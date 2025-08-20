1ac294ecf7e02fbbd20569b3700ceebf6fb35e0a
cgcb-01 <206439682+cgcb-01@users.noreply.github.com>
2025-08-20 10:14:10 +0530

    Name changed

--- Redux-toolkit_Notes/Redux-toolkit_Query/src/app/service/dummyData.js
+++ Redux-toolkit_Notes/Redux-toolkit_Query/src/app/service/dummyData.js
@@ -3,8 +3,10 @@ createApi({
   reducerPath: "products",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dummyhson.com" }), // here we need o mention the ase url from where the data will be fetched. The api build by express and MongoDB but here we will use a Dummy API
   endPoints: (builder) => ({
+    //GetAllProducts from base+endpoint url (reading) so quer for changes or updates: mutation
     getAllProducts: builder.query({
-      quer: () => "/products",
+      query: () => "/products",
     }),
   }),
 });
+export const { useGetAllProductsQuery } = productsApi; //useGetAllProductsQuery is a hook that can be used in components to fetch data
